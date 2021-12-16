36b9286e466c510a92550886b2e42bd605e4e15e
Luisa Muñoz <munozluisa643@gmail.com>
2021-12-16 16:24:55 -0500

    Author, Comments, CommentsForm, PostDetail components created, Post Folder with [slug].js created, all new components exported, recent Post and Categories implemented, header and categories fetched implemented,, posts dinamic data implemented and connected to cms

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react'
+import { getCategories } from '../services'
 import Link from 'next/link';
 
-const categories = [{name: 'Chiken', slug: 'Chiken'}, {name: 'Pork', slug: 'Pork'}]
+
+
 
 const Header = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, [])
+
     return (
         <div className="container ms-auto px-10 mb-8">
             <div className="border-b w-full inline-block boreder-blue-400 py-8">
