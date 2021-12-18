4d28da75664def3959c26e51bd9fce98eeabd6f2
Luisa Muñoz <munozluisa643@gmail.com>
2021-12-18 10:08:54 -0500

    New components: author.jsx, postDetai.jsx, FeaturedPostCard.jsx, FeaturedPost.jsx  creted, Services folder with index.js with graphql connection created and implemented, API with comments.js created and implemented, routes whit [slug].js implemented, sections folder with [slug].js created and featuredPosts carousel implemented

--- services/index.js
+++ services/index.js
@@ -39,6 +39,41 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+            post(where: {slug: $slug}){
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }
+        }
+    `;
+  
+    const result = await request(graphqlAPI, query, {slug});
+  
+    return result.post;
+  };
+
 export const getRecentPosts = async () => {
     const query = gql`
         query GetPostDetails() {
@@ -61,9 +96,9 @@ export const getRecentPosts = async () => {
     return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
-        query GetPostDetails($slug: Strign!, $categories: [String!]) {
+        query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
                 where: { slug_not: $slug, AND: {categories_some: { slug_in: $categories}}}
                 last: 3
@@ -78,7 +113,7 @@ export const getSimilarPosts = async () => {
         }
     `;
 
-    const result = await request(graphqlAPI, query);
+    const result = await request(graphqlAPI, query, { categories, slug });
 
     return result.posts;
 }
@@ -96,3 +131,56 @@ export const getCategories = async () => {
 
     return result.categories;
 }
+
+export const submitComment = async (obj) => {
+    const result = await  fetch ('/api/comments', {
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(obj),
+    })
+
+    return result.json();
+}
+
+export const getComments = async (slug) => {
+    const query = gql`
+        query GetComments($slug: String!) {
+            comments(where: {post: { slug: $slug }}) {
+                name
+                createdAt
+                comment
+            }
+        }
+    `;
+
+    const result = await request(graphqlAPI, query, {slug});
+
+    return result.comments;
+}
+
+export const getFeaturedPosts = async () => {
+    const query = gql`
+      query GetCategoryPost() {
+        posts(where: {featuredPost: true}) {
+          author {
+            name
+            photo {
+              url
+            }
+          }
+          featuredImage {
+            url
+          }
+          title
+          slug
+          createdAt
+        }
+      }   
+    `;
+  
+    const result = await request(graphqlAPI, query);
+  
+    return result.posts;
+  };
